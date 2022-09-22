e51d57cf68f79d7fc23a7c98eaf0629e01671380
booklearner <booklearner@proton.me>
2022-09-22 12:49:59 -0400

    merlin company mode

--- custom/languages.el
+++ custom/languages.el
@@ -15,6 +15,10 @@
 
 ;; config for ocaml with merlin
 (require 'merlin-company)
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'merlin-company-backend))
+; Enable company on merlin managed buffers
+(add-hook 'merlin-mode-hook 'company-mode)
 (load "/Users/booklearner/.opam/default/share/emacs/site-lisp/tuareg-site-file")
 (require 'opam-user-setup "~/src/dot-emacs-d/custom/opam-user-setup.el")
 
